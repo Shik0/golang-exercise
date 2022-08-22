bb8c6d372dbcdc64fe1b273a68a2baa505af5901
shik0 <mikailovsh@gmail.com>
2022-08-22 11:51:03 +0200

    extend struct

--- structs/main.go
+++ structs/main.go
@@ -2,13 +2,26 @@ package main
 
 import "fmt"
 
-func main() {
+type contactInfo struct {
+	email   string
+	zipCode int
+}
 
-	type person struct {
-		firstName string
-		lastName  string
+type person struct {
+	firstName string
+	lastName  string
+	contact   contactInfo
+}
+
+func main() {
+	alex := person{
+		firstName: "Alex",
+		lastName:  "John",
+		contact: contactInfo{
+			email:   "alex@gmail.com",
+			zipCode: 90000,
+		},
 	}
 
-	alex := person{firstName: "Alex", lastName: "Anderson"}
-	fmt.Println(alex)
+	fmt.Printf("%+v", alex)
 }
